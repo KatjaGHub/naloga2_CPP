12b3136e8b9ed2941f9de6be13eabe3275c4d27d
Katja <katja.slekovec@student.um.si>
2025-03-26 17:56:28 +0100

    Dodana funkcija: Dodala zahteve naloge z uporabo sortiranja

--- vaja1.cpp
+++ vaja1.cpp
@@ -1,94 +1,44 @@
 #include <fstream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-bool Branje_Stevil(vector<int>& vec, const char s[]) {
-    ifstream input(s);
-    int st;
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
+	ifstream input(s);
+	int st;
 
-    if (!input.is_open()) {
-        return false;
-    }
+	if (!input.is_open()) {
+		return false;
+	}
 
-    while (!input.eof()) {
-        input >> st;
-        vec.push_back(st);
-        while (isspace(input.peek())) input.get();
-    }
-    input.close();
-    return true;
+	while (!input.eof()) {
+		input >> st;
+		vec.push_back(st);
+		while (isspace(input.peek())) input.get();
+	}
+	input.close();
+	return true;
 }
 
 void Izpis_Stevil(int* polje, unsigned int velikost) {
-    ofstream output("out.txt");
+	ofstream output("out.txt");
 
-    for (int i = 0; i < velikost; i++)
-        output << polje[i] << ' ';
-}
-
-void CountingSort(vector<int>& A) {
-    if (A.empty()) return;
-
-    int max_val = *max_element(A.begin(), A.end());
-    int min_val = *min_element(A.begin(), A.end());
-    int range = max_val - min_val + 1;
-
-    vector<int> C(range, 0);
-    vector<int> B(A.size());
-
-    for (int num : A) {
-        C[num - min_val]++;
-    }
-
-    for (size_t i = 1; i < C.size(); i++) {
-        C[i] += C[i - 1];
-    }
-
-    for (int i = A.size() - 1; i >= 0; i--) {
-        B[C[A[i] - min_val] - 1] = A[i];
-        C[A[i] - min_val]--;
-    }
-
-    A = B;
-}
-
-void RomanSort(vector<int>& A) {
-    if (A.empty()) return;
-
-    int max_val = *max_element(A.begin(), A.end());
-    int min_val = *min_element(A.begin(), A.end());
-    int range = max_val - min_val + 1;
-
-    vector<int> C(range, 0);
-    vector<int> B;
-
-    for (int num : A) {
-        C[num - min_val]++;
-    }
-
-    for (int i = 0; i < range; i++) {
-        for (int j = 0; j < C[i]; j++) {
-            B.push_back(i + min_val);
-        }
-    }
-
-    A = B;
+	for (int i = 0; i<velikost; i++)
+		output << polje[i] << ' ';
 }
 
 int main(int argc, const char* argv[]) {
-    vector<int> A;
+	vector<int> A;
 
-    if (argc < 3) return 0;
-    if (!Branje_Stevil(A, argv[2])) return 0;
+	if (argc < 3) return 0;
+	if (!Branje_Stevil(A, argv[2])) return 0;
 
-    if (argv[1][0] == '0') {
-        CountingSort(A);
-    }
-    else {
-        RomanSort(A);
-    }
-    Izpis_Stevil(&A[0], A.size());
+	if (argv[1][0] == '0') {
+		//counting sort
+	}
+	else {
+		//Roman sort
+	}
+	Izpis_Stevil(&A[0],A.size());
 
-    return 0;
+	return 0;
 }
